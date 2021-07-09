e78315025da355d512d6cef6ce93337b734b79fa
Edward Shen <code@eddie.sh>
2021-07-09 17:32:00 -0400

    Use build script

--- build.rs
+++ build.rs
@@ -1,10 +1,25 @@
 use std::error::Error;
+use std::process::Command;
 
 use vergen::{vergen, Config, ShaKind};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Initialize vergen stuff
     let mut config = Config::default();
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
     vergen(config)?;
+
+    // Initialize SQL stuff
+    let project_root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    Command::new("mkdir")
+        .args(["cache", "--parents"])
+        .current_dir(&project_root)
+        .output()?;
+
+    Command::new("sqlite3")
+        .args(["cache/metadata.sqlite", include_str!("db_queries/init.sql")])
+        .current_dir(&project_root)
+        .output()?;
+
     Ok(())
 }
