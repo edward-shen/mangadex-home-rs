60bec5592ac93de525800d1b2d51f6d977644fa8
Edward Shen <code@eddie.sh>
2021-07-09 20:37:15 -0400

    Have build script modify .env file

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
-use std::error::Error;
+use std::path::PathBuf;
 use std::process::Command;
+use std::str::FromStr;
+use std::{error::Error, io::Write};
 
 use vergen::{vergen, Config, ShaKind};
 
@@ -20,6 +22,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         .args(["cache/metadata.sqlite", include_str!("db_queries/init.sql")])
         .current_dir(&project_root)
         .output()?;
+
+    let env_var_regex = "^DATABASE_URL=sqlite:./cache/metadata.sqlite$";
+    if !Command::new("grep")
+        .args([env_var_regex, ".env"])
+        .current_dir(&project_root)
+        .output()?
+        .status
+        .success()
+    {
+        let mut path = PathBuf::from_str(&project_root)?;
+        path.push(".env");
+
+        let mut file = std::fs::OpenOptions::new().append(true).open(path)?;
+        file.write_all(b"\nDATABASE_URL=sqlite:./cache/metadata.sqlite\n")?;
+        file.sync_all()?;
+    }
 
     Ok(())
 }
